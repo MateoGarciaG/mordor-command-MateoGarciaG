b97a83a971a8ab3e20d69ca3365e91238bce9a5c
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-05-09 16:41:44 +0200

    feat: :sparkles: Add InternationalOrder Class with their methods

---
+++ src/main/java/org/pinpong/mordorcommand/orders/InternationalOrder.java
@@ -0,0 +1,30 @@
+package org.pinpong.mordorcommand.orders;
+
+import org.pinpong.mordorcommand.interfaces.Order;
+
+public class InternationalOrder  implements Order{
+
+    private String id = null;
+    private String destiny = null;
+    private int weight = 0;
+
+    public InternationalOrder(String destiny, int weight) {
+        this.destiny = destiny;
+        this.weight = weight;
+    }
+
+
+    @Override
+    public int weight() {
+        return this.weight;
+    }
+
+    @Override
+    public String destiny() {
+        return this.destiny;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+}
