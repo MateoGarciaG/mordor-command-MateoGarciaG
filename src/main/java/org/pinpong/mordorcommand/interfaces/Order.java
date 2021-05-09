9889c732ff53c83088d42c8252dbe339fd6ffcad
MateoGarciaG <mgarciag@cifpfbmoll.eu>
2021-05-09 17:12:30 +0200

    feat: :sparkles: Add Default method in Order Interface to generate a Random ID

--- src/main/java/org/pinpong/mordorcommand/interfaces/Order.java
+++ src/main/java/org/pinpong/mordorcommand/interfaces/Order.java
@@ -1,6 +1,20 @@
 package org.pinpong.mordorcommand.interfaces;
 
+import java.util.Random;
+
 public interface Order {
     int weight();
     String destiny();
+
+    default String generatesRandomId() {
+        String ALPHANUMERIC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder id = new StringBuilder();
+        Random random = new Random();
+        while (id.length() <= 5) { // 5 WILL BE the length of the random string.
+            char getCharacter =  ALPHANUMERIC.charAt(random.nextInt(ALPHANUMERIC.length()));
+            id.append(getCharacter);
+        }
+        return id.toString();
+    }
+    
 }
